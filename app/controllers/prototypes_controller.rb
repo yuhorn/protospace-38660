b356096155073c841f4dbac7245659367497a940
yuhorn <makkachin3@gmail.com>
2023-02-23 01:26:20 +0900

    miss

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,7 +26,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless current_user == @prototype.user
+    unless current_user == prototype.user
       redirect_to root_path
     end
   end
